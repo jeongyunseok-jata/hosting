6283afafcd12de8a54b6b3ece79ff346d21ee266
jeongyunseok-jata <58690986+jeongyunseok-jata@users.noreply.github.com>
2023-06-13 00:37:27 +0900

    hosting

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -68,15 +68,15 @@ const Navbar = () => {
       className='fixed left-0 top-0 w-full z-10 ease-in duration-200'
     >
       <div className="container mx-auto">
-        <div className='max-w-[1536px] m-auto flex justify-between items-center p-1 text-white'>
+        <div className='max-w-[1536px] m-auto flex justify-between items-center p-1 font-semibold text-white'>
           <Link href='/'>
-            <h1 style={{ color: `${textColor}` }} className='font-semibold text-2xl pl-4 py-1'>
+            <h1 style={{ color: `${textColor}` }} className='text-2xl pl-4 py-3'>
               First 호스팅
             </h1>
           </Link>
-          <ul className='hidden sm:flex' style={{ color: `${textColor}` }}>
+          <ul className='hidden sm:flex'style={{ color: `${textColor}` }}>
             <li
-              className='p-4'
+              className=''
               onMouseEnter={openDropdown1}
               onMouseLeave={closeDropdown1}
             >
@@ -108,7 +108,7 @@ const Navbar = () => {
               )}
             </li>
             <li
-              className='p-4'
+              className=''
               onMouseEnter={openDropdown2}
               onMouseLeave={closeDropdown2}
             >
@@ -139,10 +139,10 @@ const Navbar = () => {
                 </div>
               )}
             </li>
-            <li className='p-4'>
+            <li className=''>
               <Link href='https://stats.uptimerobot.com/Jj0vRHBrlk'>업타임</Link>
             </li>
-            <li className='p-4'>
+            <li className=''>
               <Link href='https://pf.kakao.com/_qcQxcK'>문의하기</Link>
             </li>
           </ul>
@@ -163,18 +163,10 @@ const Navbar = () => {
                 className='h-screen w-full float-right m-0 p-0 block relative'
               ></div>
             ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-8 h-8 mr-4 mt-[-5px]"
-                viewBox="0 0 20 20"
-                fill={textColor}
-                stroke="currentColor"
-                strokeWidth="2.2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M3 12h18M3 6.5h17M3 17.5h18" />
+              <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 13 24" fill={textColor} className='w-9 h-9 mr-4'>
+                <path d="M20,11H4c-0.6,0-1,0.4-1,1s0.4,1,1,1h16c0.6,0,1-0.4,1-1S20.6,11,20,11z M4,8h16c0.6,0,1-0.4,1-1s-0.4-1-1-1H4 C3.4,6,3,6.4,3,7S3.4,8,4,8z M20,16H4c-0.6,0-1,0.4-1,1s0.4,1,1,1h16c0.6,0,1-0.4,1-1S20.6,16,20,16z" fill="currentColor"></path>
               </svg>
+
 
             )}
           </div>
@@ -191,68 +183,78 @@ const Navbar = () => {
           <div
             className={
               nav
-                ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-2/4 h-screen bg-menu text-center z-20 transition-all duration-300 transform translate-x-0'
+                ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 w-2/4 h-screen bg-menu transition-all duration-300 transform translate-x-0'
                 : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0'
             }
           >
             {nav && (
-              <ul className='w-full'>
+              <ul className='w-full text-lg font-normal'>
+                <li className='text-2xl p-7 pb-12'>
+                  <Link href='https://pf.kakao.com/_qcQxcK'>First 호스팅</Link>
+                </li>
                 <li
-                  className='p-4 text-3xl font-semibold hover:text-gray-300'
+                  className=''
                   onClick={mobileOpen1}
                 >
                   <p
                     href='/price'
-                    className='cursor-pointer'
+                    className='cursor-pointer pb-4 pl-7'
                   >
                     가격표
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='w-5 h-5 float-right mr-7 mt-2'>
+                      <path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" fill="currentColor"></path>
+                    </svg>
                   </p>
                   {isOpen1 && (
-                    <div className="relative mt-2 text-center text-xl">
-                      <ul className='w-full'>
+                    <div className="relative mt-2">
+                      <ul className='w-full pl-14 text-base'>
                         <Link href='/price'>
-                          <li className="text-neutral-300 py-[5px] px-12">가상 서버</li>
+                          <li className="pb-4">가상 서버</li>
                         </Link>
                         <Link href='/price'>
-                          <li className="text-neutral-300 py-[5px]">단독 서버</li>
+                          <li className="pb-4">단독 서버</li>
                         </Link>
                         <Link href='/price'>
-                          <li className="text-neutral-300 py-[5px]">코로케이션</li>
+                          <li className="pb-4">코로케이션</li>
                         </Link>
                       </ul>
                     </div>
                   )}
                 </li>
                 <li
-                  className='p-4 text-3xl font-semibold hover:text-gray-300'
+                  className=''
                   onClick={mobileOpen2}
                 >
                   <p
                     href=''
-                    className='cursor-pointer'
+                    className='cursor-pointer pb-4 pl-7'
                   >
                     약관
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='w-5 h-5 float-right mr-7 mt-2'>
+                      <path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" fill="currentColor"></path>
+                    </svg>
+
                   </p>
                   {isOpen2 && (
-                    <div className="relative mt-2 text-center text-xl">
+                    <div className="relative mt-2 text-base pl-14">
                       <ul className='w-full'>
                         <Link href='/price'>
-                          <li className="text-neutral-300 py-[5px] px-12">서비스 이용약관</li>
+                          <li className="pb-4">서비스 이용약관</li>
                         </Link>
                         <Link href='/price'>
-                          <li className="text-neutral-300 py-[5px]">주의사항</li>
+                          <li className="pb-4">주의사항</li>
                         </Link>
                         <Link href='/price'>
-                          <li className="text-neutral-300 py-[5px]">개인정보처리방침</li>
+                          <li className="pb-4">개인정보처리방침</li>
                         </Link>
                       </ul>
                     </div>
                   )}
                 </li>
-                <li className='p-4 text-3xl font-semibold hover:text-gray-300'>
+                <li className='pb-4 pl-7'>
                   <Link href='https://stats.uptimerobot.com/Jj0vRHBrlk'>업타임</Link>
                 </li>
-                <li className='p-4 text-3xl font-semibold hover:text-gray-300'>
+                <li className='pl-7'>
                   <Link href='https://pf.kakao.com/_qcQxcK'>문의하기</Link>
                 </li>
               </ul>
